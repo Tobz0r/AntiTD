303d575e56cfc7272ba7d411d119c2baa489a669
dv13tes <dv13tes@cs.umu.se>
2015-11-27 14:02:40 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/AntiTD/AntiTDMain.java
    	src/tiles/PathTile.java
    	src/tiles/TeleporterTile.java
    	src/tiles/TowerTile.java

--- src/AntiTD/tiles/Tile.java
+++ src/AntiTD/tiles/Tile.java
@@ -3,10 +3,7 @@ package AntiTD.tiles;
 /**
  * Created by dv13trm on 2015-11-27.
  */
+import AntiTD.Position;
 public interface Tile {
-    private Position pos;
 
-    public Tile(Position pos) {
-        this.pos = pos;
-    }
 }
