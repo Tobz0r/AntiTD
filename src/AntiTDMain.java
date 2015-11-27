495acb0d532454f2b2a8016643271e7685dde911
Mattias Edin <mattias.edin92@gmail.com>
2015-11-27 12:56:50 +0100

    First commit

--- src/AntiTDMain.java
+++ src/AntiTDMain.java
@@ -4,6 +4,7 @@
 public class AntiTDMain {
     public static void main(String[] args){
         System.out.println("Hello w1orld!");
+        new GUI();
     }
 
 }
--- src/GUI.java
+++ src/GUI.java
@@ -3,6 +3,27 @@ import javax.swing.*;
 /**
  * Created by dv13tes on 2015-11-27.
  */
-public class GUI extends JFrame {
-    super("AntiTD");
+public class GUI {
+    private JFrame frame;
+
+    public GUI() {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                frame = new JFrame("AntiTTD");
+
+                frame.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                        System.exit(0);
+                    }
+                });
+
+                JPanel menu = new Menu();
+                frame.add(menu);
+
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
 }
