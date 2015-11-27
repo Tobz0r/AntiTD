4ceed009e932dfa80fef1a13d9bd848ad6aa0bfd
dv13trm <thomrenstrom@hotmail.se>
2015-11-27 13:46:40 +0100

    min första

--- src/Position.java
+++ src/Position.java
@@ -2,4 +2,22 @@
  * Created by dv13trm on 2015-11-27.
  */
 public class Position {
+    private int x;
+    private int y;
+
+    Position(){
+        this.x=x;
+        this.y=y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public int getPos(){
+
+    }
 }
