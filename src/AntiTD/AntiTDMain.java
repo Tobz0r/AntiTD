38833c319a2850bd50098d515d089ecb0b06698c
Mattias Edin <mattias.edin92@gmail.com>
2015-11-27 15:24:15 +0100

    read file

--- src/AntiTD/AntiTDMain.java
+++ src/AntiTD/AntiTDMain.java
@@ -1,5 +1,7 @@
 package AntiTD;
 
+import java.io.IOException;
+
 /**
  * Created by dv13tes on 2015-11-27.
  */
@@ -9,6 +11,11 @@ public class AntiTDMain {
     public static void main(String[] args){
         System.out.println("Hello w1orld!");
         new GUI();
+        try {
+            new ReadXML();
+        } catch (IOException e) {
+            System.out.println("det funkar icke");
+        }
     }
 
 }
--- src/AntiTD/ReadXML.java
+++ src/AntiTD/ReadXML.java
@@ -1,33 +1,64 @@
 package AntiTD;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
 import java.io.IOException;
+
 
 /**
  * Created by dv13tes on 2015-11-27.
  */
 public class ReadXML {
+    NodeList nodeList;
+
+    private class Level {
+        String row;
+
+        public Level() {
+
+        }
+    }
+
+
     public ReadXML() throws IOException {
-        NodeList nodeList;
+
         try {
+
+            File fXmlFile = new File("levels.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inStream);
-            Element data=doc.getDocumentElement();
-            //Fixa så att tex inte flera textnoder finns efter varandra
-            data.normalize();
-            nodeList = doc.getElementsByTagName(TEA);
-        } catch (ParserConfigurationException e) {
-            throw new IOException("Unable to configure parser");
-        } catch (SAXException e) {
-            throw new IOException("Not correct format");
+            Document doc = dBuilder.parse(fXmlFile);
+
+            //optional, but recommended
+            //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("row");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("row number : " + eElement.getAttribute("number"));
+                    System.out.println("tiles : " + eElement.getElementsByTagName("tiles").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+
     }
 }
