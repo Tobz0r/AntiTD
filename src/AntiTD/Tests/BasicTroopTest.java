1bd7061345362a7b3a9656a3eabc8d33a69a6597
Mattias Edin <mattias.edin92@gmail.com>
2015-11-30 16:42:44 +0100

    tests

---
+++ src/AntiTD/Tests/BasicTroopTest.java
@@ -0,0 +1,66 @@
+package AntiTD.Tests;
+
+import AntiTD.Position;
+import AntiTD.tiles.BasicTile;
+import AntiTD.tiles.Tile;
+import AntiTD.troops.BasicTroop;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mattias on 2015-11-30.
+ */
+public class BasicTroopTest {
+
+    BasicTroop t;
+
+    @Before
+    public void setUp() throws Exception {
+        t = new BasicTroop(new Image, new BasicTile());
+    }
+
+    @Test
+    public void testTick() throws Exception {
+
+    }
+
+    @Test
+    public void testTick1() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testGetImage() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrentScore() throws Exception {
+
+    }
+
+    @Test
+    public void testAttackThis() throws Exception {
+
+    }
+
+    @Test
+    public void testIsAlive() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPosition() throws Exception {
+
+    }
+}
