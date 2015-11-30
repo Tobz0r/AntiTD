80d63eca5c16d0e7f55b6f54672a3753e890be0d
dv13tes <dv13tes@cs.umu.se>
2015-11-30 15:02:13 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/AntiTD/AntiTDMain.java
    	src/tiles/PathTile.java
    	src/tiles/TeleporterTile.java
    	src/tiles/TowerTile.java

--- src/AntiTD/tiles/Level.java
+++ src/AntiTD/tiles/Level.java
@@ -2,6 +2,7 @@ package AntiTD.tiles;
 
 import AntiTD.Position;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 /**
@@ -11,6 +12,7 @@ public class Level {
 
     private Position startPosition;
     private Position goalPosition;
+    private static Tile[][] currentMap;
     private Tile[][] map;
     private String name;
 
@@ -20,19 +22,29 @@ public class Level {
 
     public Level(String name){
         this.name=name;
-
     }
-    public void addMap(Tile[][] map){
+    public  void addMap(Tile[][] map){
         this.map=map;
     }
+    public  Tile[][] getMap(){
+        return map;
+    }
     public void setStartPosition(Position startPosition){
+
         this.startPosition=startPosition;
     }
     public void setGoalPosition(Position goalPosition){
+
         this.goalPosition=goalPosition;
     }
     public void setVictoryPoints(int victoryPoints) {
+
         this.victoryPoints = victoryPoints;
     }
-
+    public static void setCurrentMap(Tile[][] map){
+        Level.currentMap=map;
+    }
+    public static Tile[][] getCurrentMap(){
+        return currentMap;
+    }
 }
--- src/AntiTD/tiles/Tile.java
+++ src/AntiTD/tiles/Tile.java
@@ -4,6 +4,9 @@ package AntiTD.tiles;
  * Created by dv13trm on 2015-11-27.
  */
 import AntiTD.Position;
+
+import java.util.ArrayList;
+
 public abstract class Tile {
 
 
@@ -27,5 +30,25 @@ public abstract class Tile {
     public void setPosition(Position position){
         this.position=position;
     }
-
+    public Position getPosition(){
+        return position;
+    }
+    public ArrayList<Tile> getNeighbours(Tile node){
+        ArrayList<Tile> neighbours=new ArrayList<Tile>();
+        Tile[][] map=Level.getCurrentMap();
+        int row=node.getPosition().getX();
+        int column=node.getPosition().getY();
+        for(int i = -1; i < 2; i++) {
+            if (!(row+i <0 || row+i >= map.length) {
+                for (int j = -1; j < 2; j++) {
+                    if (!(column+j < 0 || column+j >= map[i].length) {
+                        if(i != 0 || j != 0) {
+                            neighbours.add(map[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        return neighbours;
+    }
 }
