cd4951615384050c1b3ffd4fc301542f3d8f70c5
Mattias Edin <mattias.edin92@gmail.com>
2015-11-30 14:28:55 +0100

    troop

--- src/AntiTD/troops/Troop.java
+++ src/AntiTD/troops/Troop.java
@@ -11,9 +11,11 @@ import java.util.Stack;
  */
 public abstract class Troop implements GameObject {
 
+    private int health;
     private int score;
     private Image img;
     private Stack<Tile> history;
+    private boolean hasReacedGoal;
 
     public Troop(Image img, Tile pos) {
         this.img = img;
@@ -26,16 +28,25 @@ public abstract class Troop implements GameObject {
     public abstract void tick();
 
     public void move() {
-        Tile[] neigbors = history.peek().getNeighbors();
-        Tile nextTile = null;
+        if (hasReacedGoal == false && health > 0) {
+            Tile[] neigbors = history.peek().getNeighbors();
+            Tile nextTile = null;
 
-        for (Tile tile : neigbors) {
-            if (history.search(tile) != -1) {
-                nextTile = tile;
+            for (Tile tile : neigbors) {
+                if (tile.isMoveable()) {
+                    if (history.search(tile) != -1) {
+                        nextTile = tile;
+                        break;
+                    }
+                }
             }
-        }
 
-        history.push(nextTile);
+            if (nextTile.isGoal()) {
+                hasReacedGoal = true;
+            }
+
+            history.push(nextTile);
+        }
     }
 
     @Override
@@ -45,7 +56,20 @@ public abstract class Troop implements GameObject {
 
     @Override
     public int getCurrentScore() {
-        return score;
+        if (hasReacedGoal && health > 0) {
+            return score;
+        } else {
+            return 0;
+        }
+    }
+
+    public boolean attackThis(int damage) {
+        health = health - damage;
+        if (health > 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     @Override
