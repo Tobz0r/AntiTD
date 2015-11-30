fb673d5d34e066445d88a7375bd55b4aa52bb285
Mattias Edin <mattias.edin92@gmail.com>
2015-11-30 13:54:19 +0100

    tower, troop

--- src/AntiTD/towers/Tower.java
+++ src/AntiTD/towers/Tower.java
@@ -1,11 +1,39 @@
 package AntiTD.towers;
 
 import AntiTD.*;
+import AntiTD.tiles.Tile;
+
+import java.awt.*;
+
 /**
  * Created by dv13tes on 2015-11-27.
  */
 public abstract class Tower implements GameObject {
+    private Tile pos;
+    private int score;
+    private Image img;
+
+    public Tower(Image img, Tile pos) {
+        this.img = img;
+        this.score = 0;
+        this.pos = pos;
+    }
 
     @Override
     public abstract void tick();
+
+    @Override
+    public Image getImage() {
+        return img;
+    }
+
+    @Override
+    public int getCurrentScore() {
+        return score;
+    }
+
+    @Override
+    public Tile getPosition() {
+        return pos;
+    }
 }
--- src/AntiTD/troops/Troop.java
+++ src/AntiTD/troops/Troop.java
@@ -1,12 +1,40 @@
 package AntiTD.troops;
 
 import AntiTD.*;
+import AntiTD.tiles.Tile;
+
+import java.awt.*;
+
 /**
  * Created by dv13trm on 2015-11-27.
  */
 public abstract class Troop implements GameObject {
 
+    private Tile pos;
+    private int score;
+    private Image img;
+
+    public Troop(Image img, Tile pos) {
+        this.img = img;
+        this.score = 0;
+        this.pos = pos;
+    }
+
     @Override
     public abstract void tick();
 
+    @Override
+    public Image getImage() {
+        return img;
+    }
+
+    @Override
+    public int getCurrentScore() {
+        return score;
+    }
+
+    @Override
+    public Tile getPosition() {
+        return pos;
+    }
 }
