e0159de458fa6fc4c3652c6bbe3f078d2e5b9039
dv13trm <thomrenstrom@hotmail.se>
2015-12-01 16:33:51 +0100

    min första

--- src/AntiTD/GUI.java
+++ src/AntiTD/GUI.java
@@ -88,6 +88,7 @@ public class GUI  {
     private void startScreen(){
         player = new JTextArea(10,20);
         player.setEditable(true);
+        
         player.setBorder(BorderFactory.createLineBorder(Color.black));
 
         startPanel = new JPanel();
