180edb158fff21cd0594a1c0009349ec46d20afa
rallmo <elfanos@gmail.com>
2015-12-01 16:21:29 +0100

    rallmoe

--- src/AntiTD/towers/BasicTower.java
+++ src/AntiTD/towers/BasicTower.java
@@ -1,7 +1,9 @@
 package AntiTD.towers;
 
+import AntiTD.Position;
 import AntiTD.tiles.Tile;
 import AntiTD.towers.*;
+import AntiTD.troops.Troop;
 
 import java.awt.*;
 
@@ -11,25 +13,89 @@ import java.awt.*;
 public class BasicTower extends Tower {
     private int damage;
     private int range;
-
-
-
-
+    private int price;
+    private Position pos;
+    private Troop target;
     public BasicTower(Image img, Tile pos) {
+
         super(img, pos);
+        setDamage(5);
+        setRange(5);
+        setPrice(1);
+        setPosition(pos.getPosition());
     }
-    public void init(){
-        damage = 5;
-        range = 5;
+    public void initScan() {
+      double distance = Integer.MAX_VALUE;
+      for(Troop troop : troops){
+
+      Troop nearUnit = null;
+      double dist = distance(troop);
+      if(dist <= getRange()) {
+        inRange.push(troop);
+          if (dist < distance) {
+           nearUnit = troop;
+            distance = dist;
+
+          }
+        }
+        if(nearUnit !=null){
+         target = nearUnit;
+        }
+      }
+    }
+    public void aggroTarget(){
+      if(target != null){
+        if(checkIfUnitIsClose(target)){
+          attack(target,getDamage());
+        }else{
+          target = null;
+          initScan();
+        }
+      }
     }
 
+    public void createTower(Image img, Tile pos){
+      Tower temp = new BasicTower(img,pos);
+      temp.init(troops, towers, pos);
+      towers.add(temp);
+    }
+    public void attack(Troop troop, int damage){
+      troop.attackThis(damage);
+    }
+    public double distance(Troop troop) {
+      return Math.hypot(troop.getPosition().getX(), troop.getPosition().getY());
+    }
+    public boolean checkIfUnitIsClose(Troop troop){
+    if(Math.hypot(troop.getPosition().getX() -getPosition().getX(), troop.getPosition().getY() - getPosition().getY()) <= getRange()){
+      return true;
+      }
+      return false;
+    }
+
+    public void setDamage(int damage){
+     this.damage = damage;
+    }
     public int getDamage(){
-        return damage;
+      return damage;
+    }
+    public void setRange(int range){
+      this.range = range;
     }
     public int getRange(){
-        return range;
+      return range;
     }
-
+    public void setPrice(int price){
+     this.price = price;
+    }
+    public int getPrice(){
+     return price;
+    }
+    public void setPosition(Position pos){
+     this.pos = pos;
+   }
+    public Position getPosition(){
+      return pos;
+    }
     public void tick(){
 
 
--- src/AntiTD/towers/FrostTower.java
+++ src/AntiTD/towers/FrostTower.java
@@ -1,6 +1,9 @@
 package AntiTD.towers;
 
+import AntiTD.Position;
 import AntiTD.tiles.Tile;
+import AntiTD.troops.Troop;
+import javafx.geometry.Pos;
 
 import java.awt.*;
 
@@ -10,17 +13,99 @@ import java.awt.*;
 public class FrostTower extends Tower{
     private int damage;
     private int range;
+    private int price;
+    private Troop target;
+    private Position pos;
+
 
 
 
 
     public FrostTower(Image img, Tile pos) {
-        super(img, pos);
+
+      super(img, pos);
+      setDamage(10);
+      setRange(10);
+      setPrice(5);
+      setPosition(pos.getPosition());
+    }
+    public void initScan() {
+      double distance = Integer.MAX_VALUE;
+      for(Troop troop : troops){
+
+        Troop nearUnit = null;
+        double dist = distance(troop);
+        if(dist <= getRange()) {
+          inRange.push(troop);
+          if (dist < distance) {
+            nearUnit = troop;
+            distance = dist;
+
+        }
+      }
+      if(nearUnit !=null){
+        target = nearUnit;
+      }
+     }
+    }
+    public void aggroTarget(){
+      if(target != null){
+       if(checkIfUnitIsClose(target)){
+          attack(target,getDamage());
+        } else{
+          target = null;
+          initScan();
+        }
+     }
+    }
+    public void createTower(Image img, Tile pos){
+
+      Tower temp = new FrostTower(img,pos);
+      temp.init(troops, towers, pos);
+      towers.add(temp);
+    }
+    public void attack(Troop troop, int damage){
+     troop.attackThis(damage);
+   }
+    public double distance(Troop troop) {
+      return Math.hypot(troop.getPosition().getX(), troop.getPosition().getY());
+    }
+
+   public boolean checkIfUnitIsClose(Troop troop){
+    if(Math.hypot(troop.getPosition().getX() -getPosition().getX(), troop.getPosition().getY() - getPosition().getY()) <= getRange()){
+      return true;
+    }
+    return false;
+    }
+    public void setDamage(int damage){
+      this.damage = damage;
+    }
+    public int getDamage(){
+      return damage;
+    }
+    public void setRange(int range){
+      this.range = range;
+    }
+    public int getRange(){
+      return range;
+    }
+    public void setPrice(int price){
+      this.price = price;
+    }
+    public int getPrice(){
+      return price;
+    }
+    public void setPosition(Position pos){
+      this.pos = pos;
+    }
+    public Position getPosition(){
+      return pos;
     }
     public void tick(){
 
-
     }
+
+
 
     @Override
     public void render(Graphics g) {
