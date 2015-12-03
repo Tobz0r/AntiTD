21aa6c6a459b4ed733588399607a473c3fd58cd2
dv13tes <dv13tes@cs.umu.se>
2015-12-03 15:38:01 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/AntiTD/AntiTDMain.java
    	src/tiles/PathTile.java
    	src/tiles/TeleporterTile.java
    	src/tiles/TowerTile.java

--- src/AntiTD/GUI.java
+++ src/AntiTD/GUI.java
@@ -94,7 +94,7 @@ public class GUI  {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 System.out.println("ELIASHEJ");
-                env.addTroops(new BasicTroop());
+                env.addTroops(new BasicTroop()); 
             }
         });
         //teleport troop button
